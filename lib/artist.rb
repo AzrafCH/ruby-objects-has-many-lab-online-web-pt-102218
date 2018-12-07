f42b8da5793397c6c4a6e272002ec9d87780f8f5
Azraf Chowdhury <achowdhur@ltu.edu>
2018-12-07 03:30:43 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,33 +1,33 @@
 class Artist 
- 
- attr_accessor :name
- 
- @@song_count = 0 
- 
- def initialize(name)
-   @name = name 
-   @songs = []
- end 
   
-  def songs
-    @songs
+  attr_accessor :name, :songs
+  
+  @@song_count = 0 
+  
+  def initialize(name)
+    @name = name 
+    @songs = [] 
   end 
- 
- def add_song(song)
-   self.songs << song
-   song.artist = self
-   @@song_count += 1 
- end 
- 
- def add_song_by_name(name)
-   song = Song.new(name)
-   self.songs << song 
-   song.artist = self
-   @@song_count += 1 
- end 
   
-  def self.song_count
+  def add_song(song)
+    song.artist = self
+    @songs << song 
+    @@song_count +=1 
+  end 
+  
+  def self.song_count 
     @@song_count
+  end 
+  
+  def add_song_by_name(name) 
+    song = Song.new(name)
+    @songs << song 
+    song.artist = self 
+    @@song_count += 1 
+  end 
+  
+  def artist_name
+    self.artist.name
   end 
   
 end 
